0ca57f38b7033d8cdbdcefd0b20a5dfff2d5e260
Abialidr <luckybelt79@gmail.com>
2024-11-25 13:00:34 +0530

    a

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@
 //   "https://ec2-13-235-133-97.ap-south-1.compute.amazonaws.com";
 
 // export const PROXY = "https://backend.aabeecure.com/";
-export const PROXY = "http://localhost:5000";
-// export const PROXY = 'https://backend.wedcellinstitute.com';
+// export const PROXY = "http://localhost:5000";
+export const PROXY = "backend.makemymattress.co.in";
